b0dc5d23e345b9d285f478da319a991b2782d4d8
Kenta OONO <oono@preferred.jp>
2016-05-21 18:58:11 +0900

    Add blank line

--- chainer/function_hooks/__init__.py
+++ chainer/function_hooks/__init__.py
@@ -1,6 +1,7 @@
 from chainer.function_hooks import debug_print
 from chainer.function_hooks import timer
 
+
 PrintHook = debug_print.PrintHook
 AccumulateTimerHook = timer.AccumulateTimerHook
 TimerHook = timer.TimerHook
