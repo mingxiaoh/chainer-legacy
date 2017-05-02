7c28aeb4fd20cce0e78f9ddb47baf49fe2f3c5c4
Yong Wu <yong.wu@intel.com>
2017-05-02 10:30:55 +0800

    Batch normalization backward with tests
    Former-commit-id: 2d820029728066406d16ea49413f28bfe076091c

--- mkldnn/batch_normalization.cc
+++ mkldnn/batch_normalization.cc
@@ -83,11 +83,6 @@ BatchNormalization<T>::BatchNormalization(double eps,
         flags_ |= use_global_stats;
     fwd_prop_kind_ = is_training ? prop_kind::forward_training : prop_kind::forward_scoring;
 
-    printf("is_training=%d\n", is_training);
-    printf("has_weights=%d\n", has_weights);
-    printf("fixed_mean_var=%d\n", fixed_mean_var);
-    printf("flags=0x%x\n", flags_);
-
     eng_.reset(new engine(engine::kind::cpu, 0));
 }
 
@@ -112,41 +107,43 @@ void BatchNormalization<T>::forward_setup(
     memory::dims src_tz = {x_d1, x_d2, x_d3, x_d4};
     memory::dims dst_tz = {y_d1, y_d2, y_d3, y_d4};
 
+    assert(W_d1 == 2);
+    assert(x_d2 == y_d2 == W_d2 == mean_d1 == var_d1);
+
     /* create memory for user data */
     LOG(INFO) << "create memory for user data";
-    user_x_mem_.reset(new memory({{{src_tz}, memory_data_type<T>(),
-                                 memory::format::nchw}, *eng_}, dummy));
-    x_md_.reset(new memory::desc({src_tz}, memory_data_type<T>(), fmt_desired));
+    user_src_mem_.reset(new memory({{{src_tz}, memory_data_type<T>(),
+                                   memory::format::nchw}, *eng_}, dummy));
+    auto src_md =  memory::desc({src_tz}, memory_data_type<T>(), fmt_desired);
 
-    user_y_mem_.reset(new memory({{{dst_tz}, memory_data_type<T>(),
-                                 memory::format::nchw }, *eng_}, dummy));
-    y_md_.reset(new memory::desc({dst_tz}, memory_data_type<T>(), memory::format::any));
+    user_dst_mem_.reset(new memory({{{dst_tz}, memory_data_type<T>(),
+                                   memory::format::nchw }, *eng_}, dummy));
 
-    if (!batch_normalization_fwd_pd_)
+    if (!fwd_pd_)
     {
-        LOG(INFO) << "batch_normalization_fwd_desc_";
-        batch_normalization_fwd_desc_.reset(new batch_normalization_forward::desc(
-                fwd_prop_kind_, *x_md_, eps_, flags_));
-        batch_normalization_fwd_pd_.reset(new batch_normalization_forward::primitive_desc(
-                *batch_normalization_fwd_desc_, *eng_));
+        LOG(INFO) << "fwd_desc_";
+        fwd_desc_.reset(new batch_normalization_forward::desc(
+                fwd_prop_kind_, src_md, eps_, flags_));
+        fwd_pd_.reset(new batch_normalization_forward::primitive_desc(
+                *fwd_desc_, *eng_));
     }
 
-    x_mem_ = user_x_mem_;
-    y_mem_ = user_y_mem_;
-    bool reorder_x_p = false;
-    bool reorder_y_p = false;
+    src_mem_ = user_src_mem_;
+    dst_mem_ = user_dst_mem_;
+    bool reorder_src_p = false;
+    bool reorder_dst_p = false;
 
     if (fmt_desired != memory::format::nchw) {
-        x_mem_.reset(new memory({{{src_tz}, memory_data_type<T>(), fmt_desired}, *eng_}));
-        reorder_x_ = reorder(*user_x_mem_, *x_mem_);
-        reorder_x_p = true;
+        src_mem_.reset(new memory({{{src_tz}, memory_data_type<T>(), fmt_desired}, *eng_}));
+        reorder_src_.reset(new reorder(*user_src_mem_, *src_mem_));
+        reorder_src_p = true;
     }
 
-    if (memory::primitive_desc(batch_normalization_fwd_pd_.get()->dst_primitive_desc())
-        != user_y_mem_->get_primitive_desc()) {
-        y_mem_.reset(new memory(batch_normalization_fwd_pd_.get()->dst_primitive_desc()));
-        reorder_y_ = reorder(*y_mem_, *user_y_mem_);
-        reorder_y_p = true;
+    if (memory::primitive_desc(fwd_pd_.get()->dst_primitive_desc())
+        != user_dst_mem_->get_primitive_desc()) {
+        dst_mem_.reset(new memory(fwd_pd_.get()->dst_primitive_desc()));
+        reorder_dst_.reset(new reorder(*dst_mem_, *user_dst_mem_));
+        reorder_dst_p = true;
     }
 
     bool scale_shift = flags_ & use_scale_shift;
@@ -154,59 +151,59 @@ void BatchNormalization<T>::forward_setup(
     bool is_training = fwd_prop_kind_ == prop_kind::forward_training;
 
     if (scale_shift)
-        W_mem_.reset(new memory(batch_normalization_fwd_pd_->weights_primitive_desc(), dummy));
+        weights_mem_.reset(new memory(fwd_pd_->weights_primitive_desc(), dummy));
 
     if (is_training || global_stats) {
-        mean_mem_.reset(new memory(batch_normalization_fwd_pd_->mean_primitive_desc(), dummy));
-        var_mem_.reset(new memory(batch_normalization_fwd_pd_->variance_primitive_desc(), dummy));
+        mean_mem_.reset(new memory(fwd_pd_->mean_primitive_desc(), dummy));
+        var_mem_.reset(new memory(fwd_pd_->variance_primitive_desc(), dummy));
     }
 
     if (!is_training && !global_stats) {
-        if (scale_shift)
+        if (scale_shift) {
             batch_normalization_fwd_.reset(new batch_normalization_forward(
-                    *batch_normalization_fwd_pd_, *x_mem_, *W_mem_, *y_mem_));
-        else
+                    *fwd_pd_, *src_mem_, *weights_mem_, *dst_mem_));
+        } else {
             batch_normalization_fwd_.reset(new batch_normalization_forward(
-                    *batch_normalization_fwd_pd_, *x_mem_, *y_mem_));
-
+                    *fwd_pd_, *src_mem_, *dst_mem_));
+        }
     } else if (global_stats) {
-        if (scale_shift)
+        if (scale_shift) {
             batch_normalization_fwd_.reset(new batch_normalization_forward(
-                    *batch_normalization_fwd_pd_, *x_mem_, (const primitive::at)*mean_mem_,
-                    (const primitive::at)*var_mem_, *W_mem_, *y_mem_));
-        else
+                    *fwd_pd_, *src_mem_, (const primitive::at)*mean_mem_,
+                    (const primitive::at)*var_mem_, *weights_mem_, *dst_mem_));
+        } else {
             batch_normalization_fwd_.reset(new batch_normalization_forward(
-                    *batch_normalization_fwd_pd_, *x_mem_, (const primitive::at)*mean_mem_,
-                    (const primitive::at)*var_mem_, *y_mem_));
+                    *fwd_pd_, *src_mem_, (const primitive::at)*mean_mem_,
+                    (const primitive::at)*var_mem_, *dst_mem_));
+        }
     } else {
         if (scale_shift) {
-            printf("xxxxxxxxxxx\n");
             batch_normalization_fwd_.reset(new batch_normalization_forward(
-                    *batch_normalization_fwd_pd_, *x_mem_, *W_mem_, *y_mem_, *mean_mem_, *var_mem_));
-        } else
+                    *fwd_pd_, *src_mem_, *weights_mem_, *dst_mem_, *mean_mem_, *var_mem_));
+        } else {
             batch_normalization_fwd_.reset(new batch_normalization_forward(
-                    *batch_normalization_fwd_pd_, *x_mem_, *y_mem_, *mean_mem_, *var_mem_));
+                    *fwd_pd_, *src_mem_, *dst_mem_, *mean_mem_, *var_mem_));
+        }
 
     }
 
-    LOG(INFO) << "    reorder_src: " << reorder_x_p;
-    LOG(INFO) << "    reorder_dst: " << reorder_y_p;
-
-    if (reorder_x_p) this->fwd_primitives_.push_back(reorder_x_);
+    if (reorder_src_p)
+        this->fwd_primitives_.push_back(*reorder_src_);
     fwd_primitives_.push_back(*batch_normalization_fwd_);
-    if (reorder_y_p) this->fwd_primitives_.push_back(reorder_y_);
+    if (reorder_dst_p)
+        this->fwd_primitives_.push_back(*reorder_dst_);
     fwd_stream_.reset(new stream(stream::kind::eager));
 }
 
 template<typename T>
 void BatchNormalization<T>::fwd_reset_mem(T* x, T* y, T* W, T* mean, T* var)
 {
-    user_x_mem_->set_data_handle(x);
-    user_y_mem_->set_data_handle(y);
+    user_src_mem_->set_data_handle(x);
+    user_dst_mem_->set_data_handle(y);
     mean_mem_->set_data_handle(mean);
     var_mem_->set_data_handle(var);
     if (flags_ & use_scale_shift)
-        W_mem_->set_data_handle(W);
+        weights_mem_->set_data_handle(W);
 }
 
 template<typename T>
@@ -232,121 +229,134 @@ void BatchNormalization<T>::forward(
     }
 }
 
-#if 0
 template<typename T>
 int BatchNormalization<T>::backward_setup(
-    T* x,  int x_d1,  int x_d2,  int x_d3,  int x_d4,
-    T* gy, int gy_d1, int gy_d2, int gy_d3, int gy_d4,
-    T* gx, int gx_d1, int gx_d2, int gx_d3, int gx_d4)
+        T* x, int x_d1, int x_d2, int x_d3, int x_d4,
+        T* W, int W_d1, int W_d2,
+        T* mean, int mean_d1, T* var, int var_d1,
+        T* gy, int gy_d1, int gy_d2, int gy_d3, int gy_d4,
+        T* gx, int gx_d1, int gx_d2, int gx_d3, int gx_d4,
+        T* gW, int gW_d1, int gW_d2)
 {
-    memory::format format;
+    memory::format fmt_desired;
     // we check AVX512 first then AVX2
     if (cpu_support_avx512_p() && (x_d2%16)==0) {
         LOG(INFO) << "backward_setup nChw16c";
-        format = memory::format::nChw16c;
+        fmt_desired = memory::format::nChw16c;
     } else if (cpu_support_avx2_p() && (x_d2%8)==0) {
-        format = memory::format::nChw8c;
+        fmt_desired = memory::format::nChw8c;
         LOG(INFO) << "backward_setup nChw8c";
     } else {
-        format = memory::format::nchw;
+        fmt_desired = memory::format::nchw;
     }
 
-    /* Backward lrn */
-    memory::dims batch_normalization_src_tz = {x_d1, x_d2, x_d3, x_d4};
-    memory::dims batch_normalization_diff_src_tz = {gx_d1, gx_d2, gx_d3, gx_d4};
-    memory::dims batch_normalization_diff_dst_tz = {gy_d1, gy_d2, gy_d3, gy_d4};
+    memory::dims src_tz = {x_d1, x_d2, x_d3, x_d4};
+    memory::dims diff_src_tz = {gx_d1, gx_d2, gx_d3, gx_d4};
+    memory::dims diff_dst_tz = {gy_d1, gy_d2, gy_d3, gy_d4};
 
-    batch_normalization_bwd_user_src_mem_.reset(new memory({{{batch_normalization_src_tz}, memory_data_type<T>(),
-        p_.data_format}, *eng_}, x));
-    batch_normalization_diff_src_mem_.reset(new memory({{{batch_normalization_diff_src_tz}, memory_data_type<T>(),
-        p_.data_format}, *eng_}, gx));
-    batch_normalization_diff_dst_mem_.reset(new memory({{{batch_normalization_diff_dst_tz}, memory_data_type<T>(),
-        p_.data_format}, *eng_}, gy));
+    bwd_user_src_mem_.reset(new memory({{{src_tz}, memory_data_type<T>(),
+                                       memory::format::nchw}, *eng_}, dummy));
+    user_diff_src_mem_.reset(new memory({{{diff_src_tz}, memory_data_type<T>(),
+                                        memory::format::nchw}, *eng_}, dummy));
+    user_diff_dst_mem_.reset(new memory({{{diff_dst_tz}, memory_data_type<T>(),
+                                        memory::format::nchw}, *eng_}, dummy));
 
-    batch_normalization_bwd_src_desc_.reset(new memory::desc({batch_normalization_src_tz},
-        memory_data_type<T>(), format));
-    batch_normalization_diff_src_desc_.reset(new memory::desc({batch_normalization_diff_src_tz},
-        memory_data_type<T>(), p_.diff_data_format));
-    batch_normalization_diff_dst_desc_.reset(new memory::desc({batch_normalization_diff_dst_tz},
-        memory_data_type<T>(), format));
+    auto bwd_src_md = memory::desc({src_tz}, memory_data_type<T>(), fmt_desired);
+    auto diff_dst_md = memory::desc({diff_dst_tz}, memory_data_type<T>(), fmt_desired);
 
-    // auto batch_normalization_src_mem_ = batch_normalization_bwd_user_src_mem_;
+    bwd_desc_.reset(new batch_normalization_backward::desc(
+            prop_kind::backward, diff_dst_md, bwd_src_md, eps_, flags_));
+    bwd_pd_.reset(new batch_normalization_backward::primitive_desc(
+            *bwd_desc_, *eng_, *fwd_pd_));
 
-    batch_normalization_bwd_desc_.reset(new batch_normalization_backward::desc(p_.aalgorithm,
-        *batch_normalization_bwd_src_desc_, *batch_normalization_diff_dst_desc_,
-        p_.local_size, p_.alpha, p_.beta,p_.k));
-    batch_normalization_bwd_pd_.reset(new batch_normalization_backward::primitive_desc(*batch_normalization_bwd_desc_, *eng_,
-        *batch_normalization_fwd_pd_));
+    bwd_src_mem_ = bwd_user_src_mem_;
+    diff_src_mem_  = user_diff_src_mem_;
+    diff_dst_mem_  = user_diff_dst_mem_;
 
-    gx_mem_   = batch_normalization_diff_src_mem_;
-    gy_mem_   = batch_normalization_diff_dst_mem_;
-    bw_x_mem_ = batch_normalization_bwd_user_src_mem_;
+    bool reorder_diff_dst_p = false;
+    bool reorder_bwd_src_p = false;
+    bool reorder_diff_src_p = false;
 
-    bool reorder_x_p = false;
-    bool reorder_y_p = false;
+    if (fmt_desired != memory::format::nchw) {
+        diff_dst_mem_.reset(new memory({{{diff_dst_tz}, memory_data_type<T>(), fmt_desired}, *eng_}));
+        reorder_diff_dst_.reset(new reorder(*user_diff_dst_mem_, *diff_dst_mem_));
+        reorder_diff_dst_p = true;
 
-    if (format != memory::format::nchw) {
-        gy_mem_.reset(new memory({{{batch_normalization_diff_dst_tz}, memory_data_type<T>(), format}, *eng_}));
-        reorder_gy_ = reorder(*batch_normalization_diff_dst_mem_, *gy_mem_);
-        reorder_y_p = true;
+        bwd_src_mem_.reset(new memory({{{src_tz}, memory_data_type<T>(), fmt_desired}, *eng_}));
+        reorder_bwd_src_.reset(new reorder(*bwd_user_src_mem_, *bwd_src_mem_));
+        reorder_bwd_src_p = true;
+
+        diff_src_mem_.reset(new memory({{{src_tz}, memory_data_type<T>(), fmt_desired}, *eng_}));
+        reorder_diff_src_.reset(new reorder(*diff_src_mem_, *user_diff_src_mem_));
+        reorder_diff_src_p = true;
     }
 
-    if (memory::primitive_desc(batch_normalization_bwd_pd_.get()->diff_src_primitive_desc())
-        != batch_normalization_diff_src_mem_->get_primitive_desc()) {
-        gx_mem_.reset(new memory(batch_normalization_bwd_pd_.get()->diff_src_primitive_desc()));
-        reorder_gx_ = reorder(*gx_mem_, *batch_normalization_diff_src_mem_);
-        reorder_x_p = true;
+    if (flags_ & use_scale_shift) {
+        diff_weights_mem_.reset(new memory(bwd_pd_->diff_weights_primitive_desc(), dummy));
+        batch_normalization_bwd_.reset(new batch_normalization_backward(
+                *bwd_pd_, *bwd_src_mem_, *mean_mem_, *var_mem_,
+                *diff_dst_mem_, *weights_mem_, *diff_src_mem_, *diff_weights_mem_));
+    } else {
+        batch_normalization_bwd_.reset(new batch_normalization_backward(
+                *bwd_pd_, *bwd_src_mem_, *mean_mem_, *var_mem_,
+                *diff_dst_mem_, *diff_src_mem_));
+
     }
 
-    LOG(INFO) << "    reorder_dst_diff: " << reorder_y_p;
-    LOG(INFO) << "    reorder_src_diff: " << reorder_x_p;
-
-    batch_normalization_bwd_.reset(new batch_normalization_backward(*batch_normalization_bwd_pd_,
-        *bw_x_mem_, *gy_mem_, *workspace_mem_,*gx_mem_));
-
-    if (reorder_y_p) bwd_primitives_.push_back(reorder_gy_);
+    if (reorder_diff_dst_p)
+        bwd_primitives_.push_back(*reorder_diff_dst_);
+    if (reorder_bwd_src_p)
+        bwd_primitives_.push_back(*reorder_bwd_src_);
     bwd_primitives_.push_back(*batch_normalization_bwd_);
-    if (reorder_x_p) bwd_primitives_.push_back(reorder_gx_);
+    if (reorder_diff_src_p)
+        bwd_primitives_.push_back(*reorder_diff_src_);
     bwd_stream_.reset(new stream(stream::kind::eager));
 
     return 0;
 }
 
 template<typename T>
-void BatchNormalization<T>::bwd_reset_mem(T* x,T* gy,T* gx, T* ws)
+void BatchNormalization<T>::bwd_reset_mem(
+    T* x, T* W, T* mean, T* var, T* gy, T* gx, T* gW)
 {
-    // batch_normalization_fwd_user_src_mem_->set_data_handle(x);
-    batch_normalization_bwd_user_src_mem_->set_data_handle(x);
-    batch_normalization_diff_src_mem_->set_data_handle(gx);
-    batch_normalization_diff_dst_mem_->set_data_handle(gy);
-    workspace_mem_->set_data_handle(ws);
-
+    bwd_user_src_mem_->set_data_handle(x);
+    mean_mem_->set_data_handle(mean);
+    var_mem_->set_data_handle(var);
+    user_diff_dst_mem_->set_data_handle(gy);
+    user_diff_src_mem_->set_data_handle(gx);
+    if (flags_ & use_scale_shift) {
+        weights_mem_->set_data_handle(W);
+        diff_weights_mem_->set_data_handle(gW);
+    }
 }
 
 template<typename T>
 int BatchNormalization<T>::backward(
-    T* x,  int x_d1,  int x_d2,  int x_d3,  int x_d4,
+    T* x, int x_d1, int x_d2, int x_d3, int x_d4,
+    T* W, int W_d1, int W_d2,
+    T* mean, int mean_d1, T* var, int var_d1,
     T* gy, int gy_d1, int gy_d2, int gy_d3, int gy_d4,
     T* gx, int gx_d1, int gx_d2, int gx_d3, int gx_d4,
-    T* ws, int ws_d)
+    T* gW, int gW_d1, int gW_d2)
 {
-    // LOG(INFO) << "backward: " << x << " : " << x_size << " : " << gy << " : " << gy_size << " : " << gx << " : " << gx_size;
     if (!bwd_stream_) {
         backward_setup(
             x, x_d1, x_d2, x_d3, x_d4,
+            W, W_d1, W_d2,
+            mean, mean_d1, var, var_d1,
             gy, gy_d1, gy_d2, gy_d3, gy_d4,
-            gx, gx_d1,gx_d2, gx_d3, gx_d4);
-        bwd_reset_mem(x, gy, gx,ws);
+            gx, gx_d1, gx_d2, gx_d3, gx_d4,
+            gW, gW_d1, gW_d2);
+        bwd_reset_mem(x, W, mean, var, gy, gx, gW);
         bwd_stream_->submit(bwd_primitives_).wait();
     }
     else {
-        bwd_reset_mem(x, gy, gx, ws);
+        bwd_reset_mem(x, W, mean, var, gy, gx, gW);
         bwd_stream_->rerun().wait();
     }
     return 0;
 }
 
-#endif
 
 template<typename T>
 BatchNormalization<T>* BatchNormalization<T>::get_forward_object(
@@ -369,18 +379,19 @@ BatchNormalization<T>* BatchNormalization<T>::get_forward_object(
     return batch_normalization_forward;
 }
 
-#if 0
 template<typename T>
 BatchNormalization<T>* BatchNormalization<T>::get_backward_object(
     int x_d1, int x_d2, int x_d3, int x_d4,
-    int n, double k, double alpha, double beta, mkldnn::algorithm alg_kind)
+    int W_d1, int W_d2, int mean_d1, double eps,
+    bool is_training, bool has_weights, bool fixed_mean_var)
 {
     auto batch_normalization_backward = dynamic_cast<BatchNormalization<T>*>(
-        LayerFactory<T>::get_instance().get_batch_normalization_layer(x_d1,x_d2,x_d3,x_d4,n,k,alpha,beta));
-    assert (batch_normalization_backward != NULL);  // we must have already done forward before
+        LayerFactory<T>::get_instance().get_batch_normalization_layer(
+            x_d1, x_d2, x_d3, x_d4, W_d1, W_d2, mean_d1, eps,
+            is_training, has_weights, fixed_mean_var));
+    assert (batch_normalization_backward != NULL);  // already done forward before
     return batch_normalization_backward;
 }
-#endif
 
 
 template class BatchNormalization<float>;
