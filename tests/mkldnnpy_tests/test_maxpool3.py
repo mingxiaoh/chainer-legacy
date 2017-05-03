d163f08d70b51e263e3f1b786fb8266894fa2464
Shilei Tian <shilei.tian@intel.com>
2017-05-03 16:07:22 +0800

    update the mkldnnpy tests with more parameters
    Former-commit-id: 9deddbabe60ec5f2f9fc2752a82b4207c4635257

--- tests/mkldnnpy_tests/test_maxpool3.py
+++ tests/mkldnnpy_tests/test_maxpool3.py
@@ -6,18 +6,21 @@ import chainer.testing.condition as condition
 from mkldnn import switch
 
 
+@testing.parameterize(*testing.product({
+    'channel': [1, 2, 4, 8, 10, 16, 24, 32, 64]
+}))
 class TestMaxpool3(unittest.TestCase):
     def setUp(self):
         self.n = 2
-        self.c = 3
+        self.c = self.channel
         self.h = 4
         self.w = 3
         self.x = np.random.rand(self.n, self.c, self.h, self.w).astype('f')
         self.x = 2 * self.x / self.x.size - 1
-
+    
     def tearDown(self):
         self.x = None
-
+    
     def check_maxpool3(self):
         switch.enable_max_pooling = True
         y1 = F.max_pooling_2d(self.x, 3, stride=2, pad=1, cover_all=False)
@@ -32,7 +35,7 @@ class TestMaxpool3(unittest.TestCase):
         y2 = F.max_pooling_2d(self.x, 3, stride=2, pad=1, cover_all=True)
         # self.assertTrue(np.array_equal(y1.data, y2.data))
         testing.assert_allclose(y1.data, y2.data)
-
+    
     @condition.retry(3)
     def test_cpu(self):
         self.check_maxpool3()
