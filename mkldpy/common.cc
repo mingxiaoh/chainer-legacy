5249753e3953382a9b67376f43113f467d6f7976
Li, Yiqiang <yiqiang.li@intel.com>
2017-05-05 01:08:04 +0800

    Turn off logging switch
    Former-commit-id: ca2d0932cd8b5f5f809269cbf66acccbec0cbe32

--- mkldpy/common.cc
+++ mkldpy/common.cc
@@ -77,7 +77,7 @@ unsigned char dummy[PAGE_SIZE] __attribute__((aligned(PAGE_SIZE)));
 
 int global_init()
 {
-    google::SetStderrLogging(0);
+    google::SetStderrLogging(1);
     google::InitGoogleLogging("mkldnnpy");
 
     LOG(INFO) << "Global Init";
