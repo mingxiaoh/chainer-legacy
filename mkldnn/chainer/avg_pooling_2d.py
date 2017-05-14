be16f1c8f5e6a1fa9b23a306d60404a27290032f
yluo29 <yuanke.luo@intel.com>
2017-05-14 20:54:02 +0800

    Add max pooling and average pooling layer for mdarry

---
+++ mkldnn/chainer/avg_pooling_2d.py
@@ -0,0 +1,26 @@
+from mkldnn.chainer.pooling_2d import *
+from mkldnn.api.support import *
+
+class AvgPooling2DMKLDNN(Pooling2DMKLDNN):
+
+    """Average pooling over a set of 2d planes."""
+
+    def forward_cpu(self, x):
+        cc = Pooling2DForward(x, pooling_avg_include_padding, ksize=(self.kh, self.kw),
+                stride=(self.sy, self.sx),
+                pad=(self.ph, self.pw), cover_all=self.cover_all,
+                pos=(self.rank, self.fanout))
+
+        self.hint = cc.hint
+        self.ws = cc.ws
+        y, = cc.execute_on()
+        return y,
+
+    def backward_cpu(self, x, gy):
+        cc = Pooling2DBackward(x, gy[0], self.hint, self.ws, pooling_avg_include_padding,
+                ksize=(self.kh, self.kw),
+                stride=(self.sy, self.sx),
+                pad=(self.ph, self.pw), cover_all=self.cover_all,
+                pos=(self.rank, self.fanout))
+        gx, = cc.execute_on()
+        return gx,
