5694e2cb5d2f432cb0b0e1c9f228bbd420ea59c0
fengyuan <feng1.yuan@intel.com>
2017-05-23 09:51:57 +0800

    Improve RTLD_GLOBAL import method. (ctypes)

--- mkldnn/__init__.py
+++ mkldnn/__init__.py
@@ -1,13 +1,8 @@
 import sys
+import ctypes
 
 # For C++ extension to work
-if sys.version_info.major >= 3 and sys.version_info.minor >= 3:
-    import os
-    ld_global_flag = os.RTLD_GLOBAL
-else:
-    import ctypes
-    ld_global_flag = ctypes.RTLD_GLOBAL
-sys.setdlopenflags(sys.getdlopenflags() | ld_global_flag)
+sys.setdlopenflags(sys.getdlopenflags() | ctypes.RTLD_GLOBAL)
 
 # API lift
 from mkldnn import api
