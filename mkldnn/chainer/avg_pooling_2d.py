78e8d1a244918b4afd9b681532894b3c575f5148
yluo29 <yuanke.luo@intel.com>
2017-08-21 10:47:04 +0800

    Fix max pooling mkldnn primitive desc issue

--- mkldnn/chainer/avg_pooling_2d.py
+++ mkldnn/chainer/avg_pooling_2d.py
@@ -17,10 +17,11 @@ class AvgPooling2DMKLDNN(Pooling2DMKLDNN):
         self.hint = cc.hint
         self.ws = cc.ws
         y, = cc.execute_on()
+        self.y = y
         return y,
 
     def backward_cpu(self, x, gy):
-        cc = Pooling2DBackward(x, gy[0], self.hint, self.ws, pooling_avg_include_padding,
+        cc = Pooling2DBackward(x, gy[0], self.hint, self.y, self.ws, pooling_avg_include_padding,
                                ksize=(self.kh, self.kw),
                                stride=(self.sy, self.sx),
                                pad=(self.ph, self.pw), cover_all=self.cover_all,
--- mkldnn/chainer/max_pooling_2d.py
+++ mkldnn/chainer/max_pooling_2d.py
@@ -15,10 +15,11 @@ class MaxPooling2DMKLDNN(Pooling2DMKLDNN):
         self.hint = cc.hint
         self.ws = cc.ws
         y, = cc.execute_on()
+        self.y = y
         return y,
 
     def backward_cpu(self, x, gy):
-        cc = Pooling2DBackward(x, gy[0], self.hint, self.ws, pooling_max,
+        cc = Pooling2DBackward(x, gy[0], self.hint, self.y, self.ws, pooling_max,
                                ksize=(self.kh, self.kw),
                                stride=(self.sy, self.sx),
                                pad=(self.ph, self.pw), cover_all=self.cover_all,
