e0c139e83d53d0f4eb02b419e48f28fdd1429f6e
fengyuan <feng1.yuan@intel.com>
2017-11-14 16:47:21 +0800

    trivia

--- dnn/include/primitives/prim_mgr/op_factory.h
+++ dnn/include/primitives/prim_mgr/op_factory.h
@@ -22,51 +22,15 @@
  *OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  *THE SOFTWARE.
  *
- *
- *######################################################################
- *# The CuPy is designed based on NumPy's API.
- *# CuPy's source code and documents contain the original NumPy ones.
- *######################################################################
- *Copyright (c) 2005-2016, NumPy Developers.
- *All rights reserved.
- *
- *Redistribution and use in source and binary forms, with or without
- *modification, are permitted provided that the following conditions are
- *met:
- *
- *    * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *
- *    * Redistributions in binary form must reproduce the above
- *       copyright notice, this list of conditions and the following
- *       disclaimer in the documentation and/or other materials provided
- *       with the distribution.
- *
- *    * Neither the name of the NumPy Developers nor the names of any
- *       contributors may be used to endorse or promote products derived
- *       from this software without specific prior written permission.
- *
- *THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- *A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- *OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- *SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- *LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- *DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- *THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- *(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- *OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *######################################################################
  */
 
 
 #ifndef _OP_FACTORY_
 #define _OP_FACTORY_
-#include <mkldnn.hpp>
+
+#include <unordered_map>
 #include <string>
 #include "op.h"
-#include <unordered_map>
 #include "config.h"
 
 extern bool enable_prim_reuse;
@@ -97,14 +61,14 @@ public:
         // just return
         if (!enable_prim_reuse)
             return;
-        
+
         auto stream_iter = map_.find(key);
         if (stream_iter == map_.end()) {
             map_[key]=op;
         } else {
             throw new std::invalid_argument("cannot set same key to a new stream");
         }
-    }; 
+    };
 
 public:
     std::unordered_map<std::string, Op<T>*> map_;
