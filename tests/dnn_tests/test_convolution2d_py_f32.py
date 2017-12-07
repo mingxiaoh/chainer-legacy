562bd8ec3906603a4750c7952e78291774a95399
Huang, Mingxiao <mingxiao.huang@intel.com>
2017-12-07 12:59:08 +0800

    add test cases into dnn_tests

---
+++ tests/dnn_tests/test_convolution2d_py_f32.py
@@ -0,0 +1,121 @@
+import sys
+import unittest
+import numpy
+import dnn._dnn
+from dnn._dnn import conv_param_t, Convolution2D_Py_F32
+
+try:
+    import testing
+    from testing import condition
+    from testing.conv import im2col_cpu
+except Exception as ex:
+    print('*** testing directory is missing: %s' % ex)
+    sys.exit(-1)
+
+
+@testing.parameterize(*testing.product({
+    'dtype': [numpy.float32, ],
+    'cover_all': [False, True],
+    'channel': [1, 2, 4, 8, 10, ],
+    'bs': [1, 2, 4, 8, 10, 16, 32, 64, ],
+    'with_bias': [True, ],
+}))
+@testing.fix_random()
+class Convolution2DPyF32(unittest.TestCase):
+
+    def setUp(self):
+        self.x_shape = (self.bs, self.channel, 224, 224)
+        self.w_shape = (self.channel, self.channel, 3, 3)
+        self.b_shape = self.channel
+
+        self.x = numpy.random.uniform(-1, 1, self.x_shape).astype(self.dtype)
+        self.x = dnn._dnn.mdarray(self.x)
+        self.w = numpy.random.uniform(-1, 1, self.w_shape).astype(self.dtype)
+        self.w = dnn._dnn.mdarray(self.w)
+        self.b = numpy.random.uniform(-1, 1, self.b_shape).astype(self.dtype)
+        self.b = dnn._dnn.mdarray(self.b)
+
+        self.cp = conv_param_t()
+        self.cp.src_d1 = self.x_shape[0]
+        self.cp.src_d2 = self.x_shape[1]
+        self.cp.src_d3 = self.x_shape[2]
+        self.cp.src_d4 = self.x_shape[3]
+        self.cp.weights_d1 = self.w_shape[0]
+        self.cp.weights_d2 = self.w_shape[1]
+        self.cp.weights_d3 = self.w_shape[2]
+        self.cp.weights_d4 = self.w_shape[3]
+        self.cp.bias_d1 = self.x_shape[1]
+        self.cp.dst_d1 = self.x_shape[0]
+        self.cp.dst_d2 = self.x_shape[1]
+        self.cp.dst_d3 = self.x_shape[2]
+        self.cp.dst_d4 = self.x_shape[3]
+        self.cp.sy = self.cp.sx = 1
+        self.cp.pad_lh = self.cp.pad_lw = self.cp.pad_rh = self.cp.pad_rw = 1
+        self.cp.with_bias = self.with_bias
+
+        stride = 1
+        pad = 1
+        dilate = 1
+        self.sy, self.sx = stride, stride
+        self.ph, self.pw = pad, pad
+        self.n = self.x_shape[0]
+        self.outc = self.w_shape[0]
+        self.outh = self.x_shape[2]
+        self.outw = self.x_shape[3]
+        self.cover_all = self.cover_all
+        self.dy, self.dx = dilate, dilate
+
+        self.gy = numpy.random.uniform(
+             -1, 1,
+             (self.n, self.outc, self.outh, self.outw)).astype(self.dtype)
+        self.gy = dnn._dnn.mdarray(self.gy)
+
+        self.check_forward_options = {'atol': 1e-3, 'rtol': 1e-2}
+        self.check_backward_options = {'atol': 1e-3, 'rtol': 1e-2}
+
+    def check_forward(self, x, w, b, cp):
+
+        if cp.with_bias:
+            y_act = Convolution2D_Py_F32.Forward(x, w, b, cp)
+        else:
+            y_act = Convolution2D_Py_F32.Forward(x, w, None, cp)
+
+        kh, kw = w.shape[2:]
+        col = im2col_cpu(
+            x, kh, kw, self.sy, self.sx, self.ph, self.pw,
+            cover_all=self.cover_all, dy=self.dy, dx=self.dx)
+        y = numpy.tensordot(
+            col, w, ((1, 2, 3), (1, 2, 3))).astype(x.dtype, copy=False)
+        if b is not None:
+            y += b
+        y_expect = numpy.rollaxis(y, 3, 1),
+        numpy.testing.assert_allclose(
+            y_act, y_expect[0], **self.check_forward_options)
+
+    def test_forward_cpu(self):
+        self.check_forward(self.x, self.w, self.b, self.cp)
+
+    def check_backward_weights(self, x, w, b, cp, gy):
+        gW_act = Convolution2D_Py_F32.BackwardWeights(x, gy, cp)
+
+        kh, kw = w.shape[2:]
+        col = im2col_cpu(
+            x, kh, kw, self.sy, self.sx, self.ph, self.pw,
+            cover_all=self.cover_all, dy=self.dy, dx=self.dx)
+
+        if (not (gy.flags.c_contiguous or gy.flags.f_contiguous) and
+                1 in gy.shape):
+            gy = numpy.ascontiguousarray(gy)
+
+        gW_expect = numpy.tensordot(
+            gy, col, ((0, 2, 3), (0, 4, 5))).astype(self.dtype, copy=False)
+        numpy.testing.assert_allclose(
+            gW_act[0][0], gW_expect[0], **self.check_backward_options)
+
+    @condition.retry(3)
+    def test_backward_cpu_weights(self):
+        print("test_backward_cpu")
+        self.check_backward_weights(self.x, self.w, self.b, self.cp, self.gy)
+
+
+testing.run_module(__name__, __file__)
---
+++ tests/dnn_tests/test_local_response_normalization_py_f32.py
@@ -0,0 +1,85 @@
+import sys
+import unittest
+
+import numpy
+import six
+import dnn._dnn
+from dnn._dnn import lrn_param_t, LocalResponseNormalization_Py_F32
+
+try:
+    import testing
+except Exception as ex:
+    print('*** testing directory is missing: %s' % ex)
+    sys.exit(-1)
+
+
+@testing.parameterize(*testing.product({
+    'dtype': [numpy.float32],
+    'shape': [(2, 7, 1, 1), (2, 7, 3, 2), ],
+}))
+class TestLocalResponseNormalizationPyF32(unittest.TestCase):
+
+    def setUp(self):
+        self.x = numpy.random.uniform(
+            -1, 1, self.shape).astype(self.dtype)
+        self.gy = numpy.random.uniform(
+            -1, 1, self.shape).astype(self.dtype)
+        self.pp = lrn_param_t()
+        self.pp.n = 5
+        self.pp.k = 2
+        self.pp.alpha = 1e-4
+        self.pp.beta = .75
+        self.pp.algo_kind = dnn._dnn.lrn_param_t.lrn_across_channels
+        self.check_forward_options = {'atol': 1e-4, 'rtol': 1e-3}
+        self.check_backward_options = {'atol': 1e-4, 'rtol': 1e-3}
+
+    def check_forward(self, x, pp):
+        x_mdarray = dnn._dnn.mdarray(x)
+        (y_act, ws) = LocalResponseNormalization_Py_F32.Forward(x_mdarray, pp)
+
+        y_expect = numpy.zeros_like(self.x)
+        for n, c, h, w in numpy.ndindex(self.x.shape):
+            s = 0
+            for i in six.moves.range(max(0, c - 2), min(7, c + 2)):
+                s += self.x[n, i, h, w] ** 2
+            denom = (2 + 1e-4 * s) ** .75
+            y_expect[n, c, h, w] = self.x[n, c, h, w] / denom
+
+        numpy.testing.assert_allclose(
+            y_expect[0], y_act[0], **self.check_forward_options)
+
+    def test_forward_cpu(self):
+        self.check_forward(self.x, self.pp)
+
+    def check_backward(self, x, gy, pp):
+        x_mdarray = dnn._dnn.mdarray(x)
+        gy_mdarray = dnn._dnn.mdarray(gy)
+        (y_act, ws) = LocalResponseNormalization_Py_F32.Forward(x_mdarray, pp)
+        gx_act = LocalResponseNormalization_Py_F32.Backward(
+            x_mdarray, gy_mdarray, ws, pp)
+
+        half_n = self.pp.n // 2
+        x2 = numpy.square(x_mdarray)
+        sum_part = x2.copy()
+        for i in six.moves.range(1, half_n + 1):
+            sum_part[:, i:] += x2[:, :-i]
+            sum_part[:, :-i] += x2[:, i:]
+        self.unit_scale = pp.k + pp.alpha * sum_part
+        self.scale = self.unit_scale ** -pp.beta
+        self.y = x_mdarray * self.scale
+
+        summand = self.y * gy / self.unit_scale
+        sum_p = summand.copy()
+        for i in six.moves.range(1, half_n + 1):
+            sum_p[:, i:] += summand[:, :-i]
+            sum_p[:, :-i] += summand[:, i:]
+
+        gx_expect = gy[0] * self.scale - 2 * pp.alpha * pp.beta * x[0] * sum_p
+        numpy.testing.assert_allclose(
+            gx_expect[0], gx_act[0], **self.check_backward_options)
+
+    def test_backward_cpu(self):
+        self.check_backward(self.x, self.gy, self.pp)
+
+
+testing.run_module(__name__, __file__)
