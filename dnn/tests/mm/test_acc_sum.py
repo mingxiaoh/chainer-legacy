ce6fd57e31748e060220445cc00951961c6560b2
yuanke <yuanke.luo@intel.com>
2017-12-15 11:33:22 +0800

    Fix pep8 issues

--- dnn/tests/mm/test_acc_sum.py
+++ dnn/tests/mm/test_acc_sum.py
@@ -1,10 +1,10 @@
 import numpy
 import dnn._dnn
 
-x1 = numpy.random.uniform(-1, 1, (3,16,2,4)).astype(numpy.float32)
-x2 = numpy.random.uniform(-1, 1, (3,16,2,4)).astype(numpy.float32)
-x3 = numpy.random.uniform(-1, 1, (3,16,2,4)).astype(numpy.float32)
-x4 = numpy.random.uniform(-1, 1, (3,16,2,4)).astype(numpy.float32)
+x1 = numpy.random.uniform(-1, 1, (3, 16, 2, 4)).astype(numpy.float32)
+x2 = numpy.random.uniform(-1, 1, (3, 16, 2, 4)).astype(numpy.float32)
+x3 = numpy.random.uniform(-1, 1, (3, 16, 2, 4)).astype(numpy.float32)
+x4 = numpy.random.uniform(-1, 1, (3, 16, 2, 4)).astype(numpy.float32)
 mx1 = dnn._dnn.mdarray(x1)
 mx2 = dnn._dnn.mdarray(x2)
 mx3 = dnn._dnn.mdarray(x3)
